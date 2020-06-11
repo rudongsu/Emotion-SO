1dd2d915b47572d591ae9059956d16df8866d996
RUDONG SU <48374785+rudongsu@users.noreply.github.com>
2020-06-11 09:53:34 +0930

    Create analysis_emotion.py
    using IBM natural_language_understanding and retrieve results

---
+++ analysis_emotion.py
@@ -0,0 +1,42 @@
+import json
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 import Features, EmotionOptions, KeywordsOptions, EntitiesOptions
+
+##
+ ### open pre-processed plain text
+ 
+files = []
+for word in open('test.txt', encoding='utf-8'):
+    files.append(word.rstrip('\n'))
+
+plaintext = " ".join(
+  [str(item) 
+    for var in files 
+             for item in var])
+
+print('file loading done')
+print(len(plaintext))
+
+##
+ ### firing IBM natural language understanding cloud service 
+ 
+authenticator = IAMAuthenticator('api')
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version='2019-07-12',
+    authenticator=authenticator
+)
+
+natural_language_understanding.set_service_url('url')
+
+response = natural_language_understanding.analyze(
+    text= plaintext,
+    features=Features(
+        entities=EntitiesOptions(emotion=True, sentiment=True, limit=2),
+        keywords=KeywordsOptions(emotion=True, sentiment=True,
+                                 limit=2)),
+        language='en'
+        ).get_result()
+        
+print('retrieving results from IBM .......')
+print(json.dumps(response, indent=2))
